83d0a0e647661342b9a2d45435840886c9372ceb
CORBY <curtisjeckan@mail.ru>
2022-07-12 17:23:59 +0500

    Выполнил 1 пункт практического задания

---
+++ OOP/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace OOP
+{
+    internal class BankAccount
+    {
+        private int accountNumber = 15674894;
+        private int balance = 15200;
+        private string savings = "Сберегательный счёт";
+
+        public string Savings { get { return savings; } set { savings = value; } }
+        public int Balance { get { return balance; } set { balance = value; } }
+        public int AccountNumber
+        {
+            get
+            {
+                return accountNumber;
+            }
+            set
+            {
+                AccountNumber = value;
+            }
+        }
+    }
+    
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP;
+class Program
+{
+    static void Main()
+    {
+        BankAccount mybankaccount = new BankAccount();
+
+        Console.WriteLine("Задание к уроку №2");
+        Console.WriteLine("Номер счёта:");
+        Console.WriteLine(mybankaccount.AccountNumber);
+        Console.WriteLine("Доступный баланс:");
+        Console.WriteLine(mybankaccount.Balance);
+        Console.WriteLine("Тип счёта:");
+        Console.WriteLine(mybankaccount.Savings);
+    }
+}
